63dbb625ae6c4fbb03bd06a109f1448a6ace5780
KevinDazaR <61398857+KevinDazaR@users.noreply.github.com>
2024-01-09 21:36:38 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// let div = document.createElement("div");
+let container = document.createElement("div");
+let contenedorCards = document.createElement("div");
+
+contenedor.classList.add("container");
+container.appendChild(contenedorCards);
+
+// FILA ROW
+let row = document.createElement("div");
+row.hasChildNodes;
+contenedorCards.appendChild("row");
+
+// COLUMNA COL
+
+let col = document.createElement;
